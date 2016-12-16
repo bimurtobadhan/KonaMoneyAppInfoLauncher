4c4289431fe2403786d4a9351a8b1a89c6ca20c3
bimurto <sksuter@gmail.com>
2016-12-17 00:06:11 +0600

    working code

---
+++ app/src/main/java/com/example/bimurto/konamoneyappinfolauncher/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bimurto.konamoneyappinfolauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startInstalledAppDetailsActivity(this);
+        finish();
+    }
+
+    public static void startInstalledAppDetailsActivity(final Activity context) {
+        if (context == null) {
+            return;
+        }
+        final Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + "com.example.bimurto.myapplication"));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(i);
+    }
+}
