270ce1cd9a20381771672f2c5b368991ddc886c9
bimurto <sksuter@gmail.com>
2016-12-17 01:41:16 +0600

    added layout

--- app/src/main/java/com/example/bimurto/konamoneyappinfolauncher/MainActivity.java
+++ app/src/main/java/com/example/bimurto/konamoneyappinfolauncher/MainActivity.java
@@ -2,30 +2,53 @@ package com.example.bimurto.konamoneyappinfolauncher;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+//    private static final String packageName = "com.example.bimurto.konamoneyappinfolauncher";
+    private static final String packageName = "com.konai.konamoney";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startInstalledAppDetailsActivity(this);
-        finish();
+        if(startInstalledAppDetailsActivity(this)) {
+            finish();
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
     }
 
-    public static void startInstalledAppDetailsActivity(final Activity context) {
-        if (context == null) {
-            return;
+    public static boolean startInstalledAppDetailsActivity(final Activity context) {
+        if (context == null ) {
+            return false;
+        }
+        if(isPackageInstalled(packageName, context.getPackageManager())){
+            return false;
         }
         final Intent i = new Intent();
         i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + "com.example.bimurto.myapplication"));
+        i.setData(Uri.parse("package:" + packageName));
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         context.startActivity(i);
+        return true;
+    }
+
+    private static boolean isPackageInstalled(String packagename, PackageManager packageManager) {
+        try {
+            packageManager.getPackageInfo(packagename, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
